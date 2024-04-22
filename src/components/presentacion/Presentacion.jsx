fea0f7276254bfac691e91d4dbf43d326041bd70
Alan Ribeca <alanribeca2003@gmail.com>
2024-04-22 17:56:19 -0300

    dandole estilos

--- src/components/presentacion/Presentacion.jsx
+++ src/components/presentacion/Presentacion.jsx
@@ -7,32 +7,48 @@ export const Presentacion = () => {
     <>
       <section className="presentacion">
         <div className="cuadrado"></div>
-        <div className="foto"></div>
+        <div className="foto">
+          <img
+            className="fotoMia"
+            src="./img/imgPortafolio.jpg"
+            alt="foto de alan"
+          />
+        </div>
 
         <article className="info">
           <h1 className="titulo">Alan Ribeca</h1>
           <p className="profesion">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-            tenetur
+            <strong className="colorTexto">Developer Front-end</strong>  apasionado y comprometido. Me encanta
+            transformar ideas y diseños en páginas web mediante <strong className="colorTexto">código limpio y
+            mantenible</strong>.
           </p>
           <p className="direccion">Rosario - Santa Fe - Argentina</p>
-          <strong className="contacto">500+ conexiones</strong>
+          <strong className="contacto">500+ Conexiones</strong>
         </article>
 
         <Botones />
 
         <section className="navegacion">
-            <ul className="ulMarcado">
-                <li className="liMarcado"> <strong className="lineaMarcadora">-</strong> Inicio</li>
-                <li className="liMarcado"> <strong className="lineaMarcadora">-</strong> Proyectos</li>
-                <li className="liMarcado"> <strong className="lineaMarcadora">-</strong> Habilidades</li>
-                <li className="liMarcado"> <strong className="lineaMarcadora">-</strong> Sobre mi</li>
-                <li className="liMarcado"> <strong className="lineaMarcadora">-</strong> Contacto</li>
-            </ul>
+          <ul className="ulMarcado">
+            <li className="liMarcado">
+              <strong className="lineaMarcadora"></strong> Inicio
+            </li>
+            <li className="liMarcado">
+              <strong className="lineaMarcadora"></strong> Proyectos
+            </li>
+            <li className="liMarcado">
+              <strong className="lineaMarcadora"></strong> Habilidades
+            </li>
+            <li className="liMarcado">
+              <strong className="lineaMarcadora"></strong> Sobre mi
+            </li>
+            <li className="liMarcado">
+              <strong className="lineaMarcadora"></strong> Contacto
+            </li>
+          </ul>
         </section>
 
         <Iconos />
-
       </section>
     </>
   );
