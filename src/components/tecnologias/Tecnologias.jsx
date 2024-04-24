4e692198a59576cfc6cb7c2cadc9860dc2b3495e
Alan Ribeca <alanribeca2003@gmail.com>
2024-04-24 19:07:54 -0300

    camabiando algunas cosas

--- src/components/tecnologias/Tecnologias.jsx
+++ src/components/tecnologias/Tecnologias.jsx
@@ -4,26 +4,26 @@ export const Tecnologias = () => {
   return (
     <section className="tecnologias">
       <div className="img">
-        <h3>Tecnologias y habilidades</h3>
+        <h3 className="h3">Tecnologias y habilidades</h3>
         <img src="..//img/html.svg" alt="logoCss" />
         <img src="..//img/css.svg" alt="" />
         <img src="..//img/sass.svg" alt="" />
         <img src="..//img/javascript.svg" alt="" />
         <img src="..//img/react.svg" alt="" />
         <img src="..//img/git.svg" alt="" />
-        <img src="..//img/github.svg" alt="" />
+        <img src="..//img/githubb.svg" alt="" />
       </div>
 
       <div className="habilidadesBlanda">
-        <h3 className="habilidadBlanda">
-          Habilidades blandas
-        </h3>
-        <p className="pBlanda">Comunicación efectiva</p>
-        <p className="pBlanda">Trabajo en equipo</p>
-        <p className="pBlanda">Resolución de problemas</p>
-        <p className="pBlanda">Creatividad</p>
-        <p className="pBlanda">Flexibilidad y adaptabilidad</p>
-        <p className="pBlanda">Aprendizaje continuo</p>  
+        <h3 className="h3">Habilidades blandas</h3>
+        <ul className="ulBlanda">
+          <li className="liBlanda">- Comunicación efectiva -</li>
+          <li className="liBlanda">- Trabajo en equipo -</li>
+          <li className="liBlanda">- Resolución de problemas -</li>
+          <li className="liBlanda">- Creatividad -</li>
+          <li className="liBlanda">- Flexibilidad y adaptabilidad -</li>
+          <li className="liBlanda">- Aprendizaje continuo -</li>
+        </ul>
       </div>
     </section>
   );
