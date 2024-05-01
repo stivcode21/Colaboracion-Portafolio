392b4f68e8ccddfbc9031354d0ada11224fef95a
Alan Ribeca <alanribeca2003@gmail.com>
2024-05-01 20:55:35 -0300

    dando estilos a los primeros moviles

--- src/components/presentacion/FlechaMovil.jsx
+++ src/components/presentacion/FlechaMovil.jsx
@@ -1,48 +1,52 @@
 export const FlechaMovil = () => {
   return (
-    <button className="button">
-      <div className="line one">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
+    <>
+      <div className="containerFlecha">
+        <button className="button">
+          <div className="line one">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+          <div className="line two">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+          <div className="line three">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+          <div className="line four">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+          <div className="line five">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+          <div className="line six">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+          <div className="line seven">
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+            <div className="round"></div>
+          </div>
+        </button>
       </div>
-      <div className="line two">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-      </div>
-      <div className="line three">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-      </div>
-      <div className="line four">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-      </div>
-      <div className="line five">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-      </div>
-      <div className="line six">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-      </div>
-      <div className="line seven">
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-        <div className="round"></div>
-      </div>
-    </button>
+    </>
   );
 };
