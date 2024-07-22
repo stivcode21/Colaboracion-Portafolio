6fce50ade6f2effbaba00d05c701aa1d3c43ea5a
Alan Ribeca <alanribeca2003@gmail.com>
2024-07-22 09:29:24 -0300

    acomodando el texto

--- src/components/informacion/TextoInfo.jsx
+++ src/components/informacion/TextoInfo.jsx
@@ -4,16 +4,12 @@ export const TextoInfo = () => {
       <section id="inicio">
         <div id="textoInicio" className="textoInfo">
           <p className="pInfo">
-          ¡Hola! Soy Alan, un desarrollador front-end capaz de crear experiencias web cautivadoras y funcionales. Con una sólida formación en tecnologías como HTML, CSS, JavaScript, React, entre otras. Junto con un ojo meticuloso para el diseño y la usabilidad, me esfuerzo por llevar las ideas desde la concepción hasta la realidad digital.
-          </p>
-          <p className="pInfo">
-            <strong className="strongInfo">¿Qué puedo hacer por ti?</strong>
-            Mi enfoque principal es desarrollar interfaces de usuario atractivas
-            y responsivas que no solo sean visualmente agradables, sino también
-            intuitivas y fáciles de usar. Desde sitios web corporativos hasta
-            aplicaciones web interactivas, tengo la experiencia y las
-            habilidades necesarias para crear soluciones que destaquen y
-            les guste a los usuarios.
+            ¡Hola! Soy Alan, un desarrollador front-end capaz de crear
+            experiencias web cautivadoras y funcionales. Con una sólida
+            formación en tecnologías como HTML, CSS, JavaScript, React, entre
+            otras. Junto con un ojo meticuloso para el diseño y la usabilidad,
+            me esfuerzo por llevar las ideas desde la concepción hasta la
+            realidad digital.
           </p>
         </div>
       </section>
